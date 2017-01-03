dd92530b0e8edbfa378177f3abec60ecc5eb51b1
Angad Nadkarni <angad@gethullo.com>
2017-01-03 17:03:55 +0530

    Removed redundant file

--- pubsub-test.js
+++
@@ -1,7 +0,0 @@
-var pubsub = require("@google-cloud/pubsub")(require("./config").gcloud);
-pubsub.topic("foo-topic").get({autoCreate: true}, function(err, topic) {
-  topic.subscribe(function(err, sub) {
-    sub.on("message", console.log);
-    topic.publish("foo");
-  });
-});
